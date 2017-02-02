2de09755907b800daf06f4a6c2648130f99e2512
dmytryshyn.romane <dmytryshyn.roman@yandex.com>
2017-02-03 01:01:12 +0200

    Created project structure.

---
+++ src/main/java/org/roman/company/search/engine/api/SearchEngineServlet.java
@@ -0,0 +1,21 @@
+package org.roman.company.search.engine.api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SearchEngineServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        System.out.println("Info");
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+        PrintWriter out = response.getWriter();
+        out.println("\nHelllloooo !!!!");
+        out.close();
+
+    }
+}
