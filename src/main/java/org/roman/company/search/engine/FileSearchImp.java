d91bfbf8716d4ccd17cf2f3cb405df5e8980f36d
dmytryshynroman <Mykhaylo.Hrytsiv@wickes.co.uk>
2017-02-06 00:07:47 +0200

    Added Searcher

--- src/main/java/org/roman/company/search/engine/FileSearchImp.java
+++ src/main/java/org/roman/company/search/engine/FileSearchImp.java
@@ -1,10 +1,17 @@
 package org.roman.company.search.engine;
 
-import org.roman.company.search.engine.bean.SearchRequest;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.roman.company.search.engine.bean.SearchRequest;
 
 public class FileSearchImp implements Searcher {
 
@@ -48,10 +55,59 @@ public class FileSearchImp implements Searcher {
 
     }
 
-    public Set<String> search(final URI filePath, final SearchRequest searchRequest) {
+    public Set<String> search(final URI filePath, final SearchRequest searchRequest) throws IOException {
 
-        /// TODO: implemet
+        Set<String> result = new LinkedHashSet<String>();
+        BufferedReader reader = new BufferedReader(new FileReader("D:\\test.txt"));
+        String line;
+        while ((line = reader.readLine()) != null) {
 
-        return null;
+            String[] words = line.split(" ");
+            for (String word : words) {
+                if(word.contains(searchRequest.getText()) && word.length() <= searchRequest.getLength()) {
+                    int limit = calculateLimit(result);
+                    if (limit >  searchRequest.getLimit()) {
+                        return result;
+                    }
+                    result.add(word);
+                }
+            }
+        }
+        return result;
     }
+
+
+    public Set<String> search1(final URI filePath, final SearchRequest searchRequest) throws IOException {
+        Set<String> result = new LinkedHashSet<String>();
+
+        final String regexPrefix = "[^\\s]*";
+        final String regex = regexPrefix + searchRequest.getText() + regexPrefix; //+ length;
+
+        BufferedReader reader = new BufferedReader(new FileReader("D:\\test.txt"));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            final Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+            final Matcher matcher = pattern.matcher(line);
+            while (matcher.find()) {
+                String word = matcher.group();
+                if(word.length() <= searchRequest.getLength()) {
+                    int limit = calculateLimit(result);
+                    if (limit >  searchRequest.getLimit()) {
+                        return result;
+                    }
+                    result.add(word);
+                }
+            }
+        }
+        return result;
+    }
+
+    private int calculateLimit(Set<String> words) {
+        int result = 0;
+        for (String value : words) {
+            result = result + value.length();
+        }
+        return result;
+    }
+
 }
--- src/main/java/org/roman/company/search/engine/Searcher.java
+++ src/main/java/org/roman/company/search/engine/Searcher.java
@@ -1,10 +1,12 @@
 package org.roman.company.search.engine;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URI;
 import java.util.Set;
 
 import org.roman.company.search.engine.bean.SearchRequest;
 
 public interface Searcher {
-    Set<String> search(URI filePath, SearchRequest searchRequest);
+    Set<String> search(URI filePath, SearchRequest searchRequest) throws IOException;
 }
